1dc984b84f9ad66cf71f241ba2c786536db2aba5
Mauricio Nascimento <mao-server@pop-os.localdomain>
2023-04-04 17:11:32 -0300

    updated script to run on docker

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,15 +2,20 @@
 
 # make O=out ARCH=arm64 r8q_defconfig
 
-export PATH=/workspace/server/tools/gcc-arm64/bin:$PATH
+export PATH=/usr/bin:$PATH
 
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       CROSS_COMPILE=aarch64-linux-gnu- \
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
                       r8q_defconfig
 
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       CROSS_COMPILE=aarch64-linux-gnu- \
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
                       Image.gz
+
 
